a8b256ec6159a1b5fcf12640b28bb906bd0926d9
Louveau Clément <157509225+valak74200@users.noreply.github.com>
2024-11-26 15:34:25 +0100

    Update _printf.c to 40 lines

--- _printf.c
+++ _printf.c
@@ -5,8 +5,7 @@
  * @format: input the format
  * Return: printed chars
  */
-int _printf(const char *format, ...)
-{
+int _printf(const char *format, ...) {
 	int count = 0, value = 0, i = 0;
 	int (*f)(va_list);
 	va_list args;
@@ -15,36 +14,27 @@ int _printf(const char *format, ...)
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
-	while (format && format[i])
-	{
-		if (format[i] != '%')
-		{
+	while (format && format[i]) {
+		if (format[i] != '%') {
 			value = write(1, &format[i], 1);
 			count = count + value;
 			i++;
-			continue;
-		}
+			continue; }
 		if (format[i] == '%')
 			f = specifiers(&format[i + 1]);
 
-		if (f)
-		{
+		if (f) {
 			value = f(args);
 			count = count + value;
 			i = i + 2;
-			continue;
-		}
+			continue; }
 		if (format[i + 1] == '\0')
 			break;
 
-		if (format[i + 1] != '\0')
-		{
+		if (format[i + 1] != '\0') {
 			value = write(1, &format[i], 1);
 			count = count + value;
 			i = i + 1;
-			continue;
-		}
-	}
+			continue; } }
 	va_end(args);
-	return (count);
-}
+	return (count); }
