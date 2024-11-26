f1b69b3bb5ddda7069776f80ba73664bdc9f0cbd
Louveau Clément <157509225+valak74200@users.noreply.github.com>
2024-11-26 15:31:37 +0100

    Remove octal and hexadecimal

--- main.c
+++ main.c
@@ -24,10 +24,6 @@ int main(void)
     printf("Negative:[%d]\n", -762534);
     _printf("Unsigned:[%u]\n", ui);
     printf("Unsigned:[%u]\n", ui);
-    _printf("Unsigned octal:[%o]\n", ui);
-    printf("Unsigned octal:[%o]\n", ui);
-    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
     _printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
