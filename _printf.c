5a0bdea87a42c31fd3d8c891f7098d69323b2c2a
valak74200 <clmentlv@outlook.fr>
2024-11-26 16:02:33 +0100

    Fix Betty errors

--- _printf.c
+++ _printf.c
@@ -10,7 +10,9 @@ int _printf(const char *format, ...)
 	int count = 0, value = 0, i = 0;
 	int (*f)(va_list);
 	va_list args;
+
 	va_start(args, format);
+
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	while (format && format[i])
--- specifiers.c
+++ specifiers.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * specifiers - selectionne la fonction appropriee pour imprimer un argument
- * @format : Caractere de format specifiant le type d'argument a imprimer
- * Return : Pointeur vers la fonction appropriee ou NULL si non trouve
+ * specifiers - print an argument
+ * @format : Format character specifying the type of argument to print
+ * Return : Pointer to the appropriate function or NULL if not found
  */
 int (*specifiers(const char *format))(va_list)
 {
-    int i;
+	int i;
 
-    static const print_t array[] = {
-	    {'c', print_char},
-	    {'s', print_string},
-	    {'d', print_int},
-	    {'i', print_int},
-	    {'%', print_pc},
-	    {'\0', NULL}
-    };
-    for (i = 0; array[i].sp != '\0'; i++)
-    {
-	    if (array[i].sp == *format)
-	    {
-		    return (array[i].f);
-	    }
-    }
-    return (NULL);
+	static const print_t array[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'d', print_int},
+		{'i', print_int},
+		{'%', print_pc},
+		{'\0', NULL}
+	};
+	for (i = 0; array[i].sp != '\0'; i++)
+	{
+		if (array[i].sp == *format)
+		{
+			return (array[i].f);
+		}
+	}
+	return (NULL);
 }
